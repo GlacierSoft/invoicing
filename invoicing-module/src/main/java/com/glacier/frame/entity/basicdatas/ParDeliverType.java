b35d25d652acc016d137a69c88bb28326c940120
wt <Administrator@HP-201406121401>
2015-01-08 13:19:30 +0800

    提交最新数据以及完成交货方式的功能

--- invoicing-module/src/main/java/com/glacier/frame/entity/basicdatas/ParDeliverType.java
+++ invoicing-module/src/main/java/com/glacier/frame/entity/basicdatas/ParDeliverType.java
@@ -2,6 +2,8 @@ package com.glacier.frame.entity.basicdatas;
 
 import java.util.Date;
 
+import com.alibaba.fastjson.annotation.JSONField;
+
 public class ParDeliverType {
     private String deliverTypeId;
 
@@ -15,11 +17,17 @@ public class ParDeliverType {
 
     private String creater;
 
+    @JSONField(format="yyyy-MM-dd HH:mm:ss")
     private Date createTime;
 
     private String updater;
 
+    @JSONField(format="yyyy-MM-dd HH:mm:ss")
     private Date updateTime;
+    
+    private String createrDisplay;
+
+    private String updaterDisplay;
 
     public String getDeliverTypeId() {
         return deliverTypeId;
@@ -92,8 +100,24 @@ public class ParDeliverType {
     public void setUpdateTime(Date updateTime) {
         this.updateTime = updateTime;
     }
+    
+    public String getCreaterDisplay() {
+		return createrDisplay;
+	}
 
-    @Override
+	public void setCreaterDisplay(String createrDisplay) {
+		this.createrDisplay = createrDisplay;
+	}
+
+	public String getUpdaterDisplay() {
+		return updaterDisplay;
+	}
+
+	public void setUpdaterDisplay(String updaterDisplay) {
+		this.updaterDisplay = updaterDisplay;
+	}
+
+	@Override
     public boolean equals(Object that) {
         if (this == that) {
             return true;
---
+++ invoicing-webapp/src/main/java/com/glacier/frame/web/controller/basicdatas/ParDeliverTypeController.java
@@ -0,0 +1,98 @@
+package com.glacier.frame.web.controller.basicdatas;
+ 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.glacier.core.controller.AbstractController;
+import com.glacier.frame.dto.query.basicdatas.ParDeliverTypeQueryDTO;
+import com.glacier.frame.entity.basicdatas.ParDeliverType;
+import com.glacier.frame.service.basicdatas.ParDeliverTypeService;
+import com.glacier.jqueryui.util.JqPager;
+
+/*** 
+ * @ClassName:  ParDeliverTypeController
+ * @Description: TODO(交货方式controller)
+ * @author wuting
+ * @email 920339213@QQ.com
+ * @date 2015-1-08
+ */
+@Controller
+@RequestMapping(value = "/parDeliverType")
+public class ParDeliverTypeController extends AbstractController{
+
+	@Autowired
+	private ParDeliverTypeService parDeliverTypeService;
+	
+	// 进入交货方式信息列表展示页面
+    @RequestMapping(value = "/index.htm")
+    private Object intoIndexPmember() {
+        ModelAndView mav = new ModelAndView("basicdatas_mgr/optgroup_mgr/parDeliverType_mgr/parDeliverType");
+        return mav;
+    } 
+     
+    
+    // 获取表格结构的所有菜单数据
+    @RequestMapping(value = "/list.json", method = RequestMethod.POST)
+    @ResponseBody
+    private Object listActionAsGridByMenuId(JqPager jqPager, ParDeliverTypeQueryDTO carTypeQueryDTO) {
+        return parDeliverTypeService.listAsGrid(jqPager, carTypeQueryDTO);
+    }
+      
+    // 进入交货方式信息Detail信息页面
+    @RequestMapping(value = "/intoDetail.htm")
+    private Object intoMemberGradeDetailPage(String deliverTypeId) { 
+    	ModelAndView mav = new ModelAndView("basicdatas_mgr/optgroup_mgr/parDeliverType_mgr/parDeliverType_detail");
+        if(StringUtils.isNotBlank(deliverTypeId)){
+            mav.addObject("deliverTypeDate", parDeliverTypeService.getParDeliverType(deliverTypeId));
+        }
+	    return mav;
+    }
+    
+    // 进入交货方式信息Form表单页面
+    @RequestMapping(value = "/intoForm.htm")
+    private Object intoGradeFormPnews(String deliverTypeId) {
+        ModelAndView mav = new ModelAndView("basicdatas_mgr/optgroup_mgr/parDeliverType_mgr/parDeliverType_form");
+        if(StringUtils.isNotBlank(deliverTypeId)){
+            mav.addObject("deliverTypeDate", parDeliverTypeService.getParDeliverType(deliverTypeId));
+        }
+        return mav;
+    }
+    
+    // 增加交货方式信息
+    @RequestMapping(value = "/add.json", method = RequestMethod.POST)
+    @ResponseBody
+    private Object addGrade(@Valid ParDeliverType parDeliverType, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {// 后台校验的错误信息
+            return returnErrorBindingResult(bindingResult);
+        }
+        return parDeliverTypeService.addParDeliverType(parDeliverType);
+    }
+    
+    //修改交货方式信息
+    @RequestMapping(value = "/edit.json", method = RequestMethod.POST)
+    @ResponseBody
+    private Object editGrade(@Valid ParDeliverType parDeliverType, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {// 后台校验的错误信息
+            return returnErrorBindingResult(bindingResult);
+        }
+        return parDeliverTypeService.editParDeliverType(parDeliverType);
+    }
+    
+    //批量删除交货方式信息
+    @RequestMapping(value = "/del.json", method = RequestMethod.POST)
+    @ResponseBody
+    public Object delGrade(@RequestParam List<String> parDeliverTypeIds,@RequestParam List<String> parDeliverTypeNames) {
+    	return parDeliverTypeService.delParDeliverType(parDeliverTypeIds, parDeliverTypeNames);
+    }
+}
